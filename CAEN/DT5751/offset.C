5084b5a700cbd26da324644b8f410567c1874cdc
Jing LIU <jing.liu@usd.edu>
2017-01-18 07:13:33 -0600

    added

---
+++ CAEN/DT5751/offset.C
@@ -0,0 +1,47 @@
+void extractOffsetData(int run=0)
+{
+   ifstream input(Form("run_%06d.000001",run), ios::binary); 
+   
+   ofstream output("offset.dat",ofstream::app);
+
+   unsigned int hdr[1000], raw;
+   input.read(reinterpret_cast<char *> &hdr,(4+10+4)*4);
+   output<<(hdr[13]&0xffff)<<" "; // set offset
+
+   int wfsize=(hdr[14]-16)/4; // individual waveform size in bytes
+   input.read(reinterpret_cast<char *> &raw,4);
+   output<<(raw&0x3FF)<<" ";
+
+   input.seekg((4+10+4)*4+wfsize); // second waveform
+   input.read(reinterpret_cast<char *> &raw,4);
+   output<<(raw&0x3FF)<<" ";
+
+   input.seekg((4+10+4)*4+wfsize*2); // third waveform
+   input.read(reinterpret_cast<char *> &raw,4);
+   output<<(raw&0x3FF)<<" ";
+
+   input.seekg((4+10+4)*4+wfsize*3); // last waveform
+   input.read(reinterpret_cast<char *> &raw,4);
+   output<<(raw&0x3FF)<<endl;
+
+   input.close();
+   output.close();
+}
+
+void offset(int lastRun=1)
+{
+   for (int run=0; run<lastRun; run++) extractOffsetData(run);
+   
+   TTree *t = new TTree("t","");
+   t->ReadFile("offset.dat","x:c0:c1:c2:c3");
+   
+   TGraph *g[4] = {0};
+   for (int i=0; i<4; i++) {
+      t->Draw(Form("c%d:x",i),Form("x>0 && x<65535 && c%d<1023",i));
+      g[i] = new TGraph(t->GetSelectedRows(), t->GetV1(), t->GetV2());
+      g[i]->SetTitle("");
+      g[i]->Draw("ap");
+      g[i]->Fit("pol1");
+      gPad->Print(Form("ch%d.png",i));
+   }
+}
---
+++ CAEN/DT5751/offset.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# take data for DC offset calibration
+val=3000
+run=0
+while [[ $val -lt 65536 ]]; do
+  cfg=`printf "run_%06d.000001.cfg" $run`
+  sed -r -e '/^channel_DC_offset/ s/[0-9]+/'$val'/' \
+    -e '/^channel_enable_recording/ s/no/yes/' daq.cfg > $cfg
+  ./daq.exe $cfg $run 1
+  val=$((val+1024))
+  run=$((run+1))
+done
+
+root -l -b -q "offset.C($((run-1)))"
+
+rm -f run_*
