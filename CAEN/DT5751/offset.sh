f745d13bc62bfb5bbe1098b0a72d88fae97ce468
Jing LIU <jing.liu@usd.edu>
2018-06-17 08:44:35 -0500

    updated

--- CAEN/DT5751/offset.sh
+++ CAEN/DT5751/offset.sh
@@ -5,7 +5,9 @@ run=0
 while [[ $val -lt 65536 ]]; do
   cfg=`printf "run_%06d.000001.cfg" $run`
   sed -r -e '/^channel_DC_offset/ s/[0-9]+/'$val'/' \
-    -e '/^channel_enable_recording/ s/no/yes/' daq.cfg > $cfg
+    -e '/^channel_enable_recording/ s/no/yes/' \
+    -e '/^channel_enable_trigger/ s/no/yes/' daq.cfg > $cfg
+  echo ./daq.exe $cfg $run 1
   ./daq.exe $cfg $run 1
   val=$((val+1024))
   run=$((run+1))
